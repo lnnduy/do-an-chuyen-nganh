73cc1cfb62d82082af248590e3800c61a3e63123
lnnd <lnnduy68@gmail.com>
2020-11-17 17:40:58 +0700

    feat: Hiển thị ip address trên titlebar

--- admin-client/app/components/Title.tsx
+++ admin-client/app/components/Title.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Titlebar, Color } from 'custom-electron-titlebar';
 import { useSelector } from 'react-redux';
 import { AppState } from '../store/app/reducer';
@@ -14,11 +14,21 @@ export default function Title(): JSX.Element {
   const { title } = useSelector<ReduxStore, AppState>((states) => states.app);
 
   useEffect(() => {
+    const nets = os.networkInterfaces();
+    let ip = null;
+    Object.keys(nets).forEach((k) => {
+      nets[k].forEach((n) => {
+        if (n.family === 'IPv4' && !n.internal) ip = n.address;
+      });
+    });
+
     const t =
       (!!title &&
         title.trim() !== '' &&
-        `${title} - Trắc nghiệm trực tuyến DNC - ${os.hostname()}`) ||
-      `Trắc nghiệm trực tuyến DNC - ${os.hostname()}`;
+        `${title} - Trắc nghiệm trực tuyến DNC - ${os.hostname()} ${
+          ip ? `[${ip}]` : ''
+        }`) ||
+      `Trắc nghiệm trực tuyến DNC - ${os.hostname()} ${ip ? `[${ip}]` : ''}`;
     document.title = t;
     appTitle.updateTitle(t);
   }, [title]);
